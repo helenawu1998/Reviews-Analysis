77142e6ee757b565c320863f0f29c1459dbe907e
Sharon Chen <sharon@Sharons-MacBook-Air.local>
2018-02-03 23:52:48 -0800

    reorganized helper functions to helper.py

---
+++ helper.py
@@ -0,0 +1,50 @@
+def load_data(train_data, test_data, skiprows = 1):
+  '''
+  Function loads training and test data stored in input files in the same folder as load_data
+  and returns x_train, y_train, and x_test in numpy ndarrays.
+
+  Inputs:
+    train_data: training_data filename
+    test_data: test_data filename
+
+  Outputs:
+    x_train: x values for training set as numpy ndarray
+    y_train: labels for x values in training set as numpy ndarray
+    x_test: x values for testing set as numpy ndarray
+  '''
+  
+  train_data = np.loadtxt(train_data, skiprows = skiprows, delimiter = ' ')
+
+  x_train = train_data[:, 1:]
+  y_train = train_data[0:,0]
+
+  x_test = np.loadtxt(test_data, skiprows = skiprows, delimiter = ' ')
+
+  return x_train, y_train, x_test
+
+# Example usage of load_data
+'''
+x_train, y_train, x_test = load_data("training_data.txt", "test_data.txt")
+'''
+
+def process_output(predictions, out_file):
+    '''
+        This function writes predictions to the output file in the correct format
+        for submission.
+        predictions: 1D array of labels for each input in the test data
+        out_file: string name of the output file
+    '''
+    f = open(out_file, 'w')
+    for i, p in enumerate(predictions):
+        if i == 0:
+            f.write("Id,Prediction\n")
+        else:
+            f.write(i+1, ",", p, "\n")
+    f.close()
+    return
+
+def error(y, y_pred):
+  '''
+  Returns classification error, given the actual and predicted y-values.
+  '''
+  return np.mean(y != y_pred)
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-import process_input
+import numpy as np
+from sklearn.model_selection import KFold
 
-print('Loading data...')
-X_train, y_train, X_test = process_input.load_data('training_data.txt', 'test_data.txt')
+import helper
 
+# Load training and test datasets.
+X_train, y_train, X_test = helper.load_data('data/training_data.txt', 'data/test_data.txt')
 
+# Perform 5-fold cross-validation.
+kf = KFold(n_splits = 5)
+inds = [ind for ind in kf.split(X_train, y_train)]
+train, val = inds[0]
+
+'''
+err_train = 0
+err_val = 0
+
+for i in range(len(inds)):
+  train, val = inds[i]
+
+  # Fit model on X_train[train], y_train[train]
+
+  # Validate on X_train[val], y_train[val]
+'''

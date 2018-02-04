f582995653a6b5815ef819815c4a8eb40d6163a6
Helena Wu <helenawu@Helenas-MacBook-Air.local>
2018-02-03 22:50:35 -0800

    Write output processing function

--- test_output.py
+++ test_output.py
@@ -1,3 +1,15 @@
 def process_output(predictions, out_file):
-    # Convert to output txt file
+    '''
+        This function writes predictions to the output file in the correct format
+        for submission.
+        predictions: 1D array of labels for each input in the test data
+        out_file: string name of the output file
+    '''
+    f = open(out_file, 'w')
+    for i, p in enumerate(predictions):
+        if i == 0:
+            f.write("Id,Prediction\n")
+        else:
+            f.write(i+1, ",", p, "\n")
+    f.close()
     return

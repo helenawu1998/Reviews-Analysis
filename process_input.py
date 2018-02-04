ec7c4ace099d23beabefca0ddbe486bb93e079bb
Pamela Zhang <pqzhang@caltech.edu>
2018-02-03 22:47:25 -0800

    load_data only reads data files within the same directory as itself. The previous version only found data files on win64. This current version will allow all OS to load the data as long it is in the same directory as the process_input.py file

--- process_input.py
+++ process_input.py
@@ -2,8 +2,8 @@ import numpy as np
 import os 
 def load_data(train_data, test_data, skiprows = 1):
 	'''
-	Function loads training and test data stored in input files and returns
-	x_train, y_train, and x_test in numpy ndarrays.
+	Function loads training and test data stored in input files in the same folder as load_data
+	and returns x_train, y_train, and x_test in numpy ndarrays.
 
 	Inputs:
 		train_data: training_data filename
@@ -14,16 +14,14 @@ def load_data(train_data, test_data, skiprows = 1):
 		y_train: labels for x values in training set as numpy ndarray
 		x_test: x values for testing set as numpy ndarray
 	'''
-	train_dir_path = os.path.dirname(os.path.realpath(train_data)) + "\\" + train_data
-	test_dir_path = os.path.dirname(os.path.realpath(test_data)) + "\\" + test_data
-
-	train_data = np.loadtxt(train_dir_path, skiprows = skiprows, delimiter = ' ')
+	
+	train_data = np.loadtxt(train_data, skiprows = skiprows, delimiter = ' ')
 
 	x_train = train_data[:, 1:]
 	y_train = train_data[0:,0]
 
-	x_test = np.loadtxt(test_dir_path, skiprows = skiprows, delimiter = ' ')
-	
+	x_test = np.loadtxt(test_data, skiprows = skiprows, delimiter = ' ')
+
 	return x_train, y_train, x_test
 
 # Example usage of load_data
